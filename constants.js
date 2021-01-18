41e0901229101a47fef549e06586426eb292dee0
zamlahani <zamlahani@gmail.com>
2021-01-18 09:57:40 +0700

    root path OK

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
 const port = process.env.PORT
+const root = __dirname
 
-module.exports = { secretKey: process.env.SECRET_KEY, port, hostDomain: process.env.DOMAIN }
+module.exports = { secretKey: process.env.SECRET_KEY, port, hostDomain: process.env.DOMAIN, root }
--- controllers/upload/index.js
+++ controllers/upload/index.js
@@ -1,13 +1,13 @@
 const moment = require('moment'); // require
 const fs = require('fs');
 const path = require('path')
-const { hostDomain } = require('../../constants');
+const { hostDomain, root } = require('../../constants');
 
 function index(req, res) {
   // console.log(req.files);
   if (req.files.file) {
     const sub = moment().format('YYYY/MM')
-    const dest = path.join('./public', req.user.id, sub,)
+    const dest = path.join('public', 'uploads', req.user.id, sub,)
     const fileName = Date.now() + '.jpg'
     fs.access(dest, function (error) {
       if (error) {
@@ -31,9 +31,10 @@ function index(req, res) {
 }
 
 function saveFile(req, res, dest, fileName) {
-  return req.files.file.mv(dest + '/' + fileName)
+  const target = `${root}/${dest}/${fileName}`
+  return req.files.file.mv(target)
     .then(() => {
-      res.send(hostDomain + '/' + dest.replace(/\\/g, '/').replace('public', 'static') + '/' + fileName)
+      res.send(`${hostDomain}/${dest.replace(/\\/g, '/').replace('public', 'static')}/${fileName}`)
     })
     .catch(err => console.error(err))
 }
